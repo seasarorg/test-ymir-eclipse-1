932a87f5d399d1b617b8484e699298bd6f4d0e09
YOKOTA, Takehiko <skirnir@gmail.com>
2008-12-04 17:56:22 +0000

    プロジェクト生成後でもfragmentを追加できるよう、skeletonの展開処理とfragmentの展開処理を分離した。
    git-svn-id: https://www.seasar.org/svn/ymir/eclipse/trunk@1991 ce2cb714-bd0d-0410-8c66-e472ade7ad34

---
+++ org.seasar.ymir.eclipse.test/src/test/java/org/seasar/ymir/eclipse/util/MavenUtilsTest.java
@@ -0,0 +1,60 @@
+package org.seasar.ymir.eclipse.util;
+
+import java.beans.Introspector;
+import java.io.InputStreamReader;
+
+import junit.framework.TestCase;
+import net.skirnir.freyja.TemplateContext;
+import net.skirnir.freyja.impl.TemplateEvaluatorImpl;
+import net.skirnir.xom.BeanAccessor;
+import net.skirnir.xom.BeanAccessorFactory;
+import net.skirnir.xom.XOMapper;
+import net.skirnir.xom.XOMapperFactory;
+import net.skirnir.xom.annotation.impl.AnnotationBeanAccessor;
+
+import org.seasar.kvasir.util.io.IOUtils;
+import org.seasar.ymir.eclipse.maven.Dependencies;
+import org.seasar.ymir.eclipse.maven.Dependency;
+import org.seasar.ymir.eclipse.maven.Project;
+import org.seasar.ymir.eclipse.maven.Repositories;
+import org.seasar.ymir.eclipse.maven.Repository;
+
+public class MavenUtilsTest extends TestCase {
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        MavenUtils.setTemplateEvaluator(new TemplateEvaluatorImpl(new PomTagEvaluator() {
+            private XOMapper mapper = XOMapperFactory.newInstance().setBeanAccessorFactory(new BeanAccessorFactory() {
+                public BeanAccessor newInstance() {
+                    return new AnnotationBeanAccessor() {
+                        @Override
+                        protected String toXMLName(String javaName) {
+                            return Introspector.decapitalize(javaName);
+                        }
+                    };
+                }
+            }).setStrict(false);
+
+            public TemplateContext newContext() {
+                return new PomTemplateContext() {
+                    @Override
+                    XOMapper getXOMapper() {
+                        return mapper;
+                    }
+                };
+            }
+        }, new PomExpressionEvaluator()));
+    }
+
+    public void testAddToPom() throws Exception {
+        Project project = new Project();
+        project.setRepositories(new Repositories(new Repository("www.seasar.org",
+                "The Seasar Foundation Maven2 Repository", "http://maven.seasar.org/maven2"), new Repository(
+                "snapshot.maven.seasar.org", "The Seasar Foundation Maven2 Snapshot Repository",
+                "http://maven.seasar.org/maven2-snapshot", true)));
+        project.setDependencies(new Dependencies(new Dependency("group1", "artifact1", "version1"), new Dependency(
+                "org.seasar.container", "s2-extension", "1.0.0")));
+        assertEquals(IOUtils.readString(getClass().getResourceAsStream("pom_expected.xml"), "UTF-8", false), MavenUtils
+                .addToPom(new InputStreamReader(getClass().getResourceAsStream("pom.xml"), "UTF-8"), project));
+    }
+}
