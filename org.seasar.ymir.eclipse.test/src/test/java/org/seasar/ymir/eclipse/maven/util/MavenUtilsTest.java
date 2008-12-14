054b2020a2f8ac6a204f3471141ca599f4d22fb4
YOKOTA, Takehiko <skirnir@gmail.com>
2008-12-14 08:24:14 +0000

    pom.xmlにprofiles,pluginRepositoriesを書けるようにした。
    git-svn-id: https://www.seasar.org/svn/ymir/eclipse/trunk@2053 ce2cb714-bd0d-0410-8c66-e472ade7ad34

--- org.seasar.ymir.eclipse.test/src/test/java/org/seasar/ymir/eclipse/maven/util/MavenUtilsTest.java
+++ org.seasar.ymir.eclipse.test/src/test/java/org/seasar/ymir/eclipse/maven/util/MavenUtilsTest.java
@@ -6,19 +6,23 @@ import java.io.InputStreamReader;
 import junit.framework.TestCase;
 import net.skirnir.freyja.TemplateContext;
 import net.skirnir.freyja.impl.TemplateEvaluatorImpl;
+import net.skirnir.xom.Attribute;
 import net.skirnir.xom.BeanAccessor;
 import net.skirnir.xom.BeanAccessorFactory;
+import net.skirnir.xom.Element;
+import net.skirnir.xom.Node;
+import net.skirnir.xom.Text;
 import net.skirnir.xom.XOMapper;
 import net.skirnir.xom.XOMapperFactory;
 import net.skirnir.xom.annotation.impl.AnnotationBeanAccessor;
 
 import org.seasar.kvasir.util.io.IOUtils;
-import org.seasar.ymir.eclipse.maven.util.MavenUtils;
-import org.seasar.ymir.eclipse.maven.util.PomExpressionEvaluator;
-import org.seasar.ymir.eclipse.maven.util.PomTagEvaluator;
-import org.seasar.ymir.eclipse.maven.util.PomTemplateContext;
 import org.seasar.ymir.vili.maven.Dependencies;
 import org.seasar.ymir.vili.maven.Dependency;
+import org.seasar.ymir.vili.maven.PluginRepositories;
+import org.seasar.ymir.vili.maven.PluginRepository;
+import org.seasar.ymir.vili.maven.Profile;
+import org.seasar.ymir.vili.maven.Profiles;
 import org.seasar.ymir.vili.maven.Project;
 import org.seasar.ymir.vili.maven.Repositories;
 import org.seasar.ymir.vili.maven.Repository;
@@ -56,8 +60,13 @@ public class MavenUtilsTest extends TestCase {
                 "The Seasar Foundation Maven2 Repository", "http://maven.seasar.org/maven2"), new Repository(
                 "snapshot.maven.seasar.org", "The Seasar Foundation Maven2 Snapshot Repository",
                 "http://maven.seasar.org/maven2-snapshot", true)));
+        project.setPluginRepositories(new PluginRepositories(new PluginRepository("codehaus snapshot repository", null,
+                "http://snapshots.repository.codehaus.org/", true)));
         project.setDependencies(new Dependencies(new Dependency("group1", "artifact1", "version1"), new Dependency(
                 "org.seasar.container", "s2-extension", "1.0.0")));
+        Profile profile = new Profile();
+        profile.addElement(new Element("id", new Attribute[0], new Node[] { new Text("executable") }));
+        project.setProfiles(new Profiles(profile));
         assertEquals(IOUtils.readString(getClass().getResourceAsStream("pom_expected.xml"), "UTF-8", false), MavenUtils
                 .addToPom(new InputStreamReader(getClass().getResourceAsStream("pom.xml"), "UTF-8"), project));
     }
