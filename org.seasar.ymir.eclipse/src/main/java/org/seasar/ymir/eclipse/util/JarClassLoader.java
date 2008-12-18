1773e63a230a10393a80f7019b2545b0748fb5dc
YOKOTA, Takehiko <skirnir@gmail.com>
2008-12-18 10:23:47 +0000

    最初のを追加。
    git-svn-id: https://www.seasar.org/svn/ymir/eclipse/trunk@2136 ce2cb714-bd0d-0410-8c66-e472ade7ad34

---
+++ org.seasar.ymir.eclipse/src/main/java/org/seasar/ymir/eclipse/util/JarClassLoader.java
@@ -0,0 +1,209 @@
+package org.seasar.ymir.eclipse.util;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Vector;
+import java.util.jar.JarEntry;
+import java.util.jar.JarInputStream;
+
+import org.seasar.kvasir.util.io.IOUtils;
+
+public class JarClassLoader extends ClassLoader {
+    private static final String SUFFIX_JAR = ".jar";
+
+    private static final String DEFAULT_CLASSESPATH = "WEB-INF/classes/";
+
+    private static final String DEFAULT_LIBPATH = "WEB-INF/lib/";
+
+    private static final String PATH_DELIMITER = "$";
+
+    private URL jarURL;
+
+    private String classesPath = DEFAULT_CLASSESPATH;
+
+    private String libPath = DEFAULT_LIBPATH;;
+
+    private Set<String> notExistPaths = Collections.synchronizedSet(new HashSet<String>());
+
+    private File tempDir;
+
+    public JarClassLoader(URL jarURL) throws IOException {
+        this.jarURL = jarURL;
+
+        initialize();
+    }
+
+    public JarClassLoader(URL jarURL, ClassLoader parent) throws IOException {
+        super(parent);
+        this.jarURL = jarURL;
+
+        initialize();
+    }
+
+    void initialize() {
+        try {
+            tempDir = File.createTempFile("JarClassLoader", ".tmp");
+            tempDir.delete();
+            tempDir.mkdirs();
+            tempDir.deleteOnExit();
+        } catch (IOException ignore) {
+        }
+    }
+
+    public void setClassesPath(String classesPath) {
+        classesPath = normalizePath(classesPath);
+    }
+
+    public void setLibPath(String libPath) {
+        libPath = normalizePath(libPath);
+    }
+
+    String normalizePath(String path) {
+        if (path == null) {
+            return null;
+        } else if (!path.endsWith("/")) {
+            return path + "/";
+        } else {
+            return path;
+        }
+    }
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        String path = name.replace('.', '/').concat(".class");
+        if (notExistPaths.contains(path)) {
+            throw new ClassNotFoundException(name);
+        }
+
+        byte[] bytes = getResourceBytes(path);
+        if (bytes == null) {
+            notExistPaths.add(path);
+            throw new ClassNotFoundException(name);
+        }
+        return defineClass(name, bytes, 0, bytes.length);
+    }
+
+    byte[] getResourceBytes(String path) {
+        InputStream is = null;
+        try {
+            is = jarURL.openStream();
+            JarInputStream jis = new JarInputStream(is);
+            for (JarEntry entry = jis.getNextJarEntry(); entry != null;) {
+                String name = entry.getName();
+                if (name.equals(classesPath + path)) {
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    IOUtils.pipe(jis, baos, false, false);
+                    return baos.toByteArray();
+                } else if (name.startsWith(libPath) && name.toLowerCase().endsWith(SUFFIX_JAR)) {
+                    JarInputStream jjis = new JarInputStream(jis);
+                    for (JarEntry e = jjis.getNextJarEntry(); e != null;) {
+                        String n = e.getName();
+                        if (n.equals(path)) {
+                            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                            IOUtils.pipe(jjis, baos, false, false);
+                            return baos.toByteArray();
+                        }
+                    }
+                }
+            }
+            return null;
+        } catch (IOException ex) {
+            return null;
+        } finally {
+            IOUtils.closeQuietly(is);
+        }
+    }
+
+    @Override
+    protected URL findResource(String path) {
+        if (notExistPaths.contains(path)) {
+            return null;
+        }
+        if (tempDir == null) {
+            return null;
+        }
+
+        InputStream is = null;
+        try {
+            is = jarURL.openStream();
+            JarInputStream jis = new JarInputStream(is);
+            for (JarEntry entry = jis.getNextJarEntry(); entry != null;) {
+                String name = entry.getName();
+                if (name.equals(classesPath + path)) {
+                    File file = new File(tempDir, classesPath + PATH_DELIMITER + path);
+                    IOUtils.pipe(jis, new FileOutputStream(file), false, true);
+                    file.deleteOnExit();
+                    return file.toURI().toURL();
+                } else if (name.startsWith(libPath) && name.toLowerCase().endsWith(SUFFIX_JAR)) {
+                    JarInputStream jjis = new JarInputStream(jis);
+                    for (JarEntry e = jjis.getNextJarEntry(); e != null;) {
+                        String n = e.getName();
+                        if (n.equals(path)) {
+                            File file = new File(tempDir, name + PATH_DELIMITER + path);
+                            IOUtils.pipe(jis, new FileOutputStream(file), false, true);
+                            file.deleteOnExit();
+                            return file.toURI().toURL();
+                        }
+                    }
+                }
+            }
+        } catch (IOException ignore) {
+        } finally {
+            IOUtils.closeQuietly(is);
+        }
+
+        notExistPaths.add(path);
+        return null;
+    }
+
+    @Override
+    protected Enumeration<URL> findResources(String path) throws IOException {
+        Vector<URL> urlList = new Vector<URL>();
+
+        if (!notExistPaths.contains(path) && tempDir != null) {
+            InputStream is = null;
+            try {
+                is = jarURL.openStream();
+                JarInputStream jis = new JarInputStream(is);
+                for (JarEntry entry = jis.getNextJarEntry(); entry != null;) {
+                    String name = entry.getName();
+                    if (name.equals(classesPath + path)) {
+                        File file = new File(tempDir, classesPath + PATH_DELIMITER + path);
+                        IOUtils.pipe(jis, new FileOutputStream(file), false, true);
+                        file.deleteOnExit();
+                        urlList.add(file.toURI().toURL());
+                    } else if (name.startsWith(libPath) && name.toLowerCase().endsWith(SUFFIX_JAR)) {
+                        JarInputStream jjis = new JarInputStream(jis);
+                        for (JarEntry e = jjis.getNextJarEntry(); e != null;) {
+                            String n = e.getName();
+                            if (n.equals(path)) {
+                                File file = new File(tempDir, name + PATH_DELIMITER + path);
+                                IOUtils.pipe(jis, new FileOutputStream(file), false, true);
+                                file.deleteOnExit();
+                                urlList.add(file.toURI().toURL());
+                                break;
+                            }
+                        }
+                    }
+                }
+                return null;
+            } finally {
+                IOUtils.closeQuietly(is);
+            }
+        }
+
+        if (urlList.isEmpty()) {
+            notExistPaths.add(path);
+        }
+
+        return urlList.elements();
+    }
+}
