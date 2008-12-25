13f54c95f1cb5a14042115b43a8be7cd096baa8a
YOKOTA, Takehiko <skirnir@gmail.com>
2008-12-25 07:04:22 +0000

    定数を追加。
    git-svn-id: https://www.seasar.org/svn/ymir/eclipse/trunk@2279 ce2cb714-bd0d-0410-8c66-e472ade7ad34

--- vili-api/src/main/java/org/seasar/ymir/vili/Globals.java
+++ vili-api/src/main/java/org/seasar/ymir/vili/Globals.java
@@ -2,4 +2,8 @@ package org.seasar.ymir.vili;
 
 public interface Globals {
     String PLUGIN_ID = "org.seasar.ymir.eclipse";
+
+    String NATURE_ID_VILIPROJECT = PLUGIN_ID + ".viliProjectNature";
+
+    String NATURE_ID_YMIRPROJECT = PLUGIN_ID + ".ymirProjectNature";
 }
