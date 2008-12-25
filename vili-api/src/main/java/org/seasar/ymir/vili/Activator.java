26d33a6dced2ab688df92f94c150a02111430f9d
YOKOTA, Takehiko <skirnir@gmail.com>
2008-12-25 15:44:38 +0000

    ログに出力するためのAPIを追加した。
    git-svn-id: https://www.seasar.org/svn/ymir/eclipse/trunk@2316 ce2cb714-bd0d-0410-8c66-e472ade7ad34

---
+++ vili-api/src/main/java/org/seasar/ymir/vili/Activator.java
@@ -0,0 +1,40 @@
+package org.seasar.ymir.vili;
+
+import org.eclipse.core.runtime.ILog;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+
+public class Activator {
+    private static final String CLASS_ACTIVATOR = "org.seasar.ymir.eclipse.Activator";
+
+    private static final String METHOD_GETDEFAULT = "getDefault";
+
+    private static final String METHOD_GETLOG = "getLog";
+
+    private Activator() {
+    }
+
+    public static void log(Throwable t) {
+        log("Exception has occured", t);
+    }
+
+    public static void log(String message) {
+        log(message, null);
+    }
+
+    public static void log(String message, Throwable t) {
+        getLog().log(new Status(IStatus.ERROR, Globals.PLUGIN_ID, message, t));
+    }
+
+    public static ILog getLog() {
+        try {
+            Class<?> activatorClass = Class.forName(CLASS_ACTIVATOR);
+            Object activator = activatorClass.getMethod(METHOD_GETDEFAULT)
+                    .invoke(null);
+            return (ILog) activatorClass.getMethod(METHOD_GETLOG).invoke(
+                    activator);
+        } catch (Throwable t) {
+            throw new RuntimeException(t);
+        }
+    }
+}
