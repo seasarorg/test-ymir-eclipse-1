d77261702f4304828e19a8f91845a69e3de10e60
YOKOTA, Takehiko <skirnir@gmail.com>
2009-01-08 17:08:36 +0000

    ・diconファイルをマージできるようにした。 ・今まではテンプレートだけがマージ対象であったが、テンプレート以外のリソースでもexpansion.merge.includesにマッチするものはマージするようにした。 ・デフォルトで**/*.xpropertiesをテンプレートとみなすようにした。 ・デフォルトで**/*.dicon, **/*.prefsをマージするようにした。
    以上に伴い、Viliテンプレートのバージョンを0.2.0に上げた。
    
    
    git-svn-id: https://www.seasar.org/svn/ymir/eclipse/trunk@2573 ce2cb714-bd0d-0410-8c66-e472ade7ad34

--- org.seasar.ymir.eclipse.test/src/test/java/org/seasar/ymir/eclipse/impl/ProjectBuilderImplTest.java
+++ org.seasar.ymir.eclipse.test/src/test/java/org/seasar/ymir/eclipse/impl/ProjectBuilderImplTest.java
@@ -1,22 +1,18 @@
 package org.seasar.ymir.eclipse.impl;
 
-import java.beans.Introspector;
 import java.io.InputStreamReader;
 
 import junit.framework.TestCase;
-import net.skirnir.freyja.TemplateContext;
-import net.skirnir.freyja.impl.TemplateEvaluatorImpl;
 import net.skirnir.xom.Attribute;
-import net.skirnir.xom.BeanAccessor;
-import net.skirnir.xom.BeanAccessorFactory;
 import net.skirnir.xom.Element;
 import net.skirnir.xom.Node;
 import net.skirnir.xom.Text;
-import net.skirnir.xom.XOMapper;
-import net.skirnir.xom.XOMapperFactory;
-import net.skirnir.xom.annotation.impl.AnnotationBeanAccessor;
 
 import org.seasar.kvasir.util.io.IOUtils;
+import org.seasar.ymir.vili.model.dicon.Component;
+import org.seasar.ymir.vili.model.dicon.Components;
+import org.seasar.ymir.vili.model.dicon.Include;
+import org.seasar.ymir.vili.model.dicon.Meta;
 import org.seasar.ymir.vili.model.maven.Dependencies;
 import org.seasar.ymir.vili.model.maven.Dependency;
 import org.seasar.ymir.vili.model.maven.PluginRepositories;
@@ -34,30 +30,9 @@ public class ProjectBuilderImplTest extends TestCase {
     protected void setUp() throws Exception {
         super.setUp();
         target = new ProjectBuilderImpl(null);
-        target.setTemplateEvaluator(new TemplateEvaluatorImpl(new PomTagEvaluator() {
-            private XOMapper mapper = XOMapperFactory.newInstance().setBeanAccessorFactory(new BeanAccessorFactory() {
-                public BeanAccessor newInstance() {
-                    return new AnnotationBeanAccessor() {
-                        @Override
-                        protected String toXMLName(String javaName) {
-                            return Introspector.decapitalize(javaName);
-                        }
-                    };
-                }
-            }).setStrict(false);
-
-            public TemplateContext newContext() {
-                return new PomTemplateContext() {
-                    @Override
-                    XOMapper getXOMapper() {
-                        return mapper;
-                    }
-                };
-            }
-        }, new PomExpressionEvaluator()));
     }
 
-    public void testAddToPom() throws Exception {
+    public void testMergePom() throws Exception {
         Project project = new Project();
         project.setRepositories(new Repositories(new Repository("www.seasar.org",
                 "The Seasar Foundation Maven2 Repository", "http://maven.seasar.org/maven2"), new Repository(
@@ -71,6 +46,45 @@ public class ProjectBuilderImplTest extends TestCase {
         profile.addElement(new Element("id", new Attribute[0], new Node[] { new Text("executable") }));
         project.setProfiles(new Profiles(profile));
         assertEquals(IOUtils.readString(getClass().getResourceAsStream("pom_expected.xml"), "UTF-8", false), target
-                .addToPom(new InputStreamReader(getClass().getResourceAsStream("pom.xml"), "UTF-8"), project));
+                .mergePom(new InputStreamReader(getClass().getResourceAsStream("pom.xml"), "UTF-8"), project));
+    }
+
+    public void testMergeDicon1_混合パターン() throws Exception {
+        Components dicon = new Components();
+        dicon.setComponents(new Component("com.example.Hoe"), new Component("com.example.Hoe2"), new Component(
+                "com.example.Class1", "class1"), new Component("com.example.Class2", "class2"));
+        dicon.setIncludes(new Include("path1"), new Include("path2"));
+        dicon.setMetas(new Meta(), new Meta("meta1"), new Meta("meta2"));
+        assertEquals(IOUtils.readString(getClass().getResourceAsStream("dicon1_expected.dicon"), "UTF-8", false),
+                target
+                        .mergeDicon(new InputStreamReader(getClass().getResourceAsStream("dicon1.dicon"), "UTF-8"),
+                                dicon));
+    }
+
+    public void testMergeDicon2_componentやmetaの前にincludeが挿入されること_最初がcomponent() throws Exception {
+        Components dicon = new Components();
+        dicon.setIncludes(new Include("path"));
+        assertEquals(IOUtils.readString(getClass().getResourceAsStream("dicon2_expected.dicon"), "UTF-8", false),
+                target
+                        .mergeDicon(new InputStreamReader(getClass().getResourceAsStream("dicon2.dicon"), "UTF-8"),
+                                dicon));
+    }
+
+    public void testMergeDicon3_componentやmetaの前にincludeが挿入されること_最初がmeta() throws Exception {
+        Components dicon = new Components();
+        dicon.setIncludes(new Include("path"));
+        assertEquals(IOUtils.readString(getClass().getResourceAsStream("dicon3_expected.dicon"), "UTF-8", false),
+                target
+                        .mergeDicon(new InputStreamReader(getClass().getResourceAsStream("dicon3.dicon"), "UTF-8"),
+                                dicon));
+    }
+
+    public void testMergeDicon4_componentやmetaの前にincludeが挿入されること_componentもmetaもない() throws Exception {
+        Components dicon = new Components();
+        dicon.setIncludes(new Include("path"));
+        assertEquals(IOUtils.readString(getClass().getResourceAsStream("dicon4_expected.dicon"), "UTF-8", false),
+                target
+                        .mergeDicon(new InputStreamReader(getClass().getResourceAsStream("dicon4.dicon"), "UTF-8"),
+                                dicon));
     }
 }
