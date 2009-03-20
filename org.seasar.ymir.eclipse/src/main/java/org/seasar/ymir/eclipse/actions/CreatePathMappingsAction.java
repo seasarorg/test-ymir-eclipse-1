6ad332146457a0efc65ddefd527c267389a9d867
YOKOTA, Takehiko <skirnir@gmail.com>
2009-03-20 18:21:03 +0000

    パスのマッピング情報を生成する画面をキックするためのメニューを追加した。
    git-svn-id: https://www.seasar.org/svn/ymir/eclipse/trunk@3039 ce2cb714-bd0d-0410-8c66-e472ade7ad34

---
+++ org.seasar.ymir.eclipse/src/main/java/org/seasar/ymir/eclipse/actions/CreatePathMappingsAction.java
@@ -0,0 +1,12 @@
+package org.seasar.ymir.eclipse.actions;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+
+public class CreatePathMappingsAction extends AbstractOpenURLAction {
+    @Override
+    protected void processResource(IProject project, IResource resource) throws Exception {
+        openUrl(getBaseURL(getPreferenceStore(project)).toString()
+                + "?__ymir__task=systemConsole&__ymir__subTask=createPathMappings&__ymir__method=GET");
+    }
+}
